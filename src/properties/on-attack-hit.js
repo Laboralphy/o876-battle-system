2af99f1551b3017385df4fbbe9557088e1a91cd8
ralphy <raphael.marandet@gmail.com>
2025-01-22 11:51:26 +0100

    test pass / seems to work. need more work

--- src/properties/on-attack-hit.js
+++ src/properties/on-attack-hit.js
@@ -1,34 +1,111 @@
-const CONSTS = require("../consts");
+const CONSTS = require('../consts')
+
+case BASE_AILMENT_DURATION = 3
 
 /**
  * This property will be applied an effect on anybody hit by attack
  * @param property {RBSProperty}
- * @param chance {string} Dice expression
  * @param savingThrow {string} ability
- * @param dc {number}
- * @param extraParams {string}
+ * @param extraParams {object}
  * @param ailment {string}
  */
-function init ({ property, ailment, chance = '', savingThrow = '', dc = 0,  extraParams = '' }) {
+function init ({ property, ailment, savingThrow = '', ...extraParams }) {
     if (!CONSTS[ailment]) {
         throw new ReferenceError('unknown ailment ' + ailment)
     }
     property.data.ailment = ailment
     property.data.savingThrow = savingThrow
-    property.data.dc = dc
-    property.data.chance = chance
     property.data.extraParams = extraParams
 }
 
-function attack ({ property, item, creature, manager, attack }) {
-    const oDicer = attack.attacker
-    if (property.data.chance) {
-        if (oDicer.dice.roll(property.data.chance) > 1) {
-            return
+function _createAilmentEffect (property, oSource, oEffectProcessor) {
+    const {
+        amp,
+        data: {
+            ailment,
+            extraParams: {
+                subtype = CONSTS.EFFECT_SUBTYPE_MAGICAL,
+                duration = Infinity,
+                ...extraParams
+            }
+        }
+    } = property
+    let oEffect
+    const nAmp = oSource.dice.roll(amp)
+    switch (ailment) {
+        case CONSTS.ON_ATTACK_HIT_ABILITY_DRAIN: {
+            const { ability } = extraParams
+            oEffect = oEffectProcessor.createEffect(CONSTS.EFFECT_ABILITY_MODIFIER, nAmp, { ability })
+            oEffect.subtype = subtype
+            // Ici revoir les tags des effect : se servir des tags pour la stackabilité
+            // stack rules : replace, prevent, update(amp, duration)
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_ATTACK_DRAIN: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_ARMOR_CLASS_DRAIN: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_BLINDNESS: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_CONFUSION: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_DISEASE: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_DOOM: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_FEAR: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_LEVEL_DRAIN: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_POISON: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_PARALYSIS: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_PETRIFICATION: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_SILENCE: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_SLOW: {
+            break
+        }
+        case CONSTS.ON_ATTACK_HIT_STUN: {
+            break
         }
     }
-    if (property.data.savingThrow) {
+}
 
+/**
+ *
+ * @param property {RBSProperty}
+ * @param item {RBSItem}
+ * @param creature {Creature}
+ * @param manager {Manager}
+ * @param attack {AttackOutcome}
+ */
+function attack ({ property, item, creature, manager, attack }) {
+    const sDefensiveAbility = property.data.savingThrow
+    if (sDefensiveAbility) {
+        const attacker = attack.attacker
+        const target = attack.target
+        const sOffensiveAbility = attack.ability
+        const dc = attacker.getters.getSpellDifficultyClass[sOffensiveAbility]
+        const { success } = target.rollSavingThrow(sDefensiveAbility, dc)
+        if (!success) {
+            // applying ailment
+            const oAilment
+        }
     }
 }
 
