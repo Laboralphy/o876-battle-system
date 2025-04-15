e9dc526a6cc8f7de51c4f0348a5d0797fc45ae33
ralphy <raphael.marandet@gmail.com>
2025-04-15 11:54:41 +0200

    new combat simulator

--- src/advantages/attack-roll.js
+++ src/advantages/attack-roll.js
@@ -1,7 +1,20 @@
 const CONSTS = require('../consts');
 const { aggregateModifiers } = require('../libs/aggregator');
 
+/**
+ * Usually, advantages are granted on attack when the attacker is unseen by its target, or the target is unable to
+ * properly defend itself.
+ *
+ * @type {{[p: string]: function(AttackOutcome): boolean}}
+ */
 const oAdvantages = {
+    /**
+     * An effect or item property may cause advantage.
+     * A short duration effect like "true strike" will grant advantage
+     * An item having attack advantage property would be considered as unfair
+     * @param attackOutcome {AttackOutcome}
+     * @returns {boolean}
+     */
     [CONSTS.ADV_ATTACK_PROPERTY_EFFECT]: attackOutcome => {
         const oAttacker = attackOutcome.attacker;
         const sAttackType = attackOutcome.attackType;
@@ -17,14 +30,29 @@ const oAdvantages = {
         }).count > 0;
     },
 
+    /**
+     * Attacking a disabled target (unable to fight) grants an advantage
+     * @param attackOutcome {AttackOutcome}
+     * @returns {boolean}
+     */
     [CONSTS.ADV_ATTACK_TARGET_DISABLED]: attackOutcome =>
         !attackOutcome.target.getters.getCapabilitySet.has(CONSTS.CAPABILITY_FIGHT),
 
+    /**
+     * Attacking an unaware target (because attacker is invisible, stealth, or in dark area) grants an advantage
+     * @param attackOutcome {AttackOutcome}
+     * @returns {boolean}
+     */
     [CONSTS.ADV_ATTACK_UNDETECTED_BY_TARGET]: attackOutcome =>
         attackOutcome.target.getCreatureVisibility(attackOutcome.attacker) !== CONSTS.CREATURE_VISIBILITY_VISIBLE
 };
 
 const oDisadvantages = {
+    /**
+     * Some effects impose a disadvantage
+     * @param attackOutcome {AttackOutcome}
+     * @returns {boolean}
+     */
     [CONSTS.DIS_ATTACK_PROPERTY_EFFECT]: attackOutcome => {
         const oAttacker = attackOutcome.attacker;
         const sAttackType = attackOutcome.attackType;
@@ -40,25 +68,55 @@ const oDisadvantages = {
         }).count > 0;
     },
 
+    /**
+     * Attacking an undetected target (invisible, or in dark area) imposes a disadvantage
+     * @param attackOutcome {AttackOutcome}
+     * @returns {boolean}
+     */
     [CONSTS.DIS_ATTACK_TARGET_UNDETECTED]: attackOutcome =>
         attackOutcome.attacker.getCreatureVisibility(attackOutcome.target) !== CONSTS.CREATURE_VISIBILITY_VISIBLE,
 
+    /**
+     * Attacking when poisoned imposes a disadvantage
+     * @param attackOutcome {AttackOutcome}
+     * @returns {*}
+     */
     [CONSTS.DIS_ATTACK_POISONED]: attackOutcome =>
         attackOutcome.attacker.getters.getConditionSet.has(CONSTS.CONDITION_POISONED),
 
+    /**
+     * Attacking when rooted imposes a disadvantage
+     * @param attackOutcome {AttackOutcome}
+     * @returns {*}
+     */
     [CONSTS.DIS_ATTACK_ROOTED]: attackOutcome =>
         attackOutcome.attacker.getters.getConditionSet.has(CONSTS.CONDITION_RESTRAINED),
 
+    /**
+     * Attacking when confused imposes a disadvantage
+     * @param attackOutcome {AttackOutcome}
+     * @returns {*}
+     */
     [CONSTS.DIS_ATTACK_CONFUSED]: attackOutcome =>
         attackOutcome.attacker.getters.getConditionSet.has(CONSTS.CONDITION_CONFUSED),
 
+    /**
+     * Attacking when wearing armor and/or shield when not proficient imposes a disadvantage
+     * @param attackOutcome
+     * @returns {boolean}
+     */
     [CONSTS.DIS_ATTACK_BAD_EQUIPMENT]: attackOutcome => {
         const eqp = attackOutcome.attacker.getters.isEquipmentProficient;
         return !(eqp[CONSTS.EQUIPMENT_SLOT_CHEST] && eqp[CONSTS.EQUIPMENT_SLOT_SHIELD]);
     },
 
+    /**
+     * Attacking with ranged weapon in a windy environment imposes a disadvantage
+     * @param attackOutcome {boolean}
+     * @returns {boolean}
+     */
     [CONSTS.DIS_ATTACK_WINDY_ENVIRONMENT]: attackOutcome =>
-        attackOutcome.attacker.getters.getEnvironment[CONSTS.DIS_ATTACK_WINDY_ENVIRONMENT] &&
+        !!attackOutcome.attacker.getters.getEnvironment[CONSTS.DIS_ATTACK_WINDY_ENVIRONMENT] &&
         (
             attackOutcome.attackType === CONSTS.ATTACK_TYPE_RANGED ||
             attackOutcome.attackType === CONSTS.ATTACK_TYPE_RANGED_TOUCH
